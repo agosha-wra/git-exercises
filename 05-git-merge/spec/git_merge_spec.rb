a6c6b673946e789b03573cfc3bdc2b9df5c6750c
Antoine Goeuriot <antoine.goeuriot@gmail.com>
2021-03-30 14:58:02 +0200

    added merge exercise and fixed start_again bug by deleting the .git folder first

---
+++ 05-git-merge/spec/git_merge_spec.rb
@@ -0,0 +1,27 @@
+require 'git'
+
+describe 'The Git Repository' do
+  before(:all) do
+    @g = Git.open('./')
+  end
+
+  it 'should exist' do
+    expect(@g).to be_instance_of(Git::Base)
+  end
+
+  it 'should have a master branch with everything in feature-3 to feature-3a' do
+    expect(@g.branch('master').contains?('feature-3a')).to be true
+    expect(@g.branch('master').contains?('feature-3')).to be true
+  end
+
+  it 'should have a master branch with everything in feature-1 to feature-1b' do
+    expect(@g.branch('master').contains?('feature-1')).to be true
+    expect(@g.branch('master').contains?('feature-1a')).to be true
+    expect(@g.branch('master').contains?('feature-1b')).to be true
+  end
+
+  it 'should have a master branch with everything in feature-2 to feature-2a' do
+    expect(@g.branch('master').contains?('feature-2')).to be true
+    expect(@g.branch('master').contains?('feature-2a')).to be true
+  end
+end
