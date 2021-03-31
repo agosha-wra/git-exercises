0dfa1930ec0968944cee1f3ee0219cc2cd4e4669
Antoine Goeuriot <antoine.goeuriot@gmail.com>
2021-03-31 15:53:42 +0200

    exercises spec and tests done for last exercises

--- 07-git-remote-fetch-pull/README.md
+++ 07-git-remote-fetch-pull/README.md
@@ -1,10 +1,21 @@
 ## Specs
 - Before the exercise, run 'rake start'
 
+- You are working on a project with a coworker. 
+- You are both working on features and sharing your work through a remote, called 'origin' in your repo.
+
+- Your coworker has pushed new features to the remote and you want them to work on your machine: 
+    + Create a new local branch called feature-2, from master, and pull the branch called feature-2 in the remote
+    + Create a new local branch called feature-1, from master, and pull the branch called feature-1 in the remote
+
+- You have a new feature ready for review, time to push it: 
+    + Create a new local branch called feature-4, from master, create a file, commit it and push it to the remote's feature-4 branch. 
+
 ## How to test
 - Just run the 'rake' command, when everything is green, you can go to the next exercise :)
 - Made a mess ? just 'rake start_again'
 
 ## What is important here 
-- 
-
+- Understanding the point of a remote and how it works.
+- Git pull and what it does
+- Git push and what it does
--- 07-git-remote-fetch-pull/spec/git_remote_fetch_pull_spec.rb
+++ 07-git-remote-fetch-pull/spec/git_remote_fetch_pull_spec.rb
@@ -9,17 +9,25 @@ describe 'The Git Repository' do
     expect(@g).to be_instance_of(Git::Base)
   end
 
-  it 'should have the feature-3 branch' do
-    expect(@g.branches.any? { |b| b.name == 'feature-3' }).to be true
-  end 
-
-  it 'should have the feature-2a branch' do 
-    expect(@g.branches.any? { |b| b.name == 'feature-2a' }).to be true
+  it 'should have a feature-2 branch that is the same as origin/feature-2' do
+    expect(@g.branches.any? { |b| b.name == 'feature-2' }).to be true
+    expect(@g.diff('feature-2', 'origin/feature-2').size == 0).to be true
   end
 
-  it 'should not have other branches from the remote' do
+  it 'should have a feature-3 branch that is the same as origin/feature-3' do
+    expect(@g.branches.any? { |b| b.name == 'feature-3' }).to be true
+    expect(@g.diff('feature-3', 'origin/feature-3').size == 0).to be true
+  end
+
+  it 'should not have a feature-1 branch' do
     expect(@g.branches.any? { |b| b.name == 'feature-1' }).to be false
-    expect(@g.branches.any? { |b| b.name == 'feature-3a' }).to be false
-    expect(@g.branches.any? { |b| b.name == 'feature-2' }).to be false
-  end 
+  end
+
+  it 'should have a feature-4 branch' do
+    expect(@g.branches.any? { |b| b.name == 'feature-4' }).to be true
+  end
+
+  it 'should have the changes in feature-4 pushed to origin/feature-4' do
+    expect(@g.diff('feature-4', 'origin/feature-4').size == 0).to be true
+  end
 end
